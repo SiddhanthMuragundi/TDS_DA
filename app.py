2deade7c1f3940a22baccf4cc23afcfe1fa8802f
Siddhanth Muragundi <118447607+SiddhanthMuragundi@users.noreply.github.com>
2025-08-12 19:12:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, UploadFile, File, Request, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -2048,4 +2049,5 @@ async def aianalyst(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
